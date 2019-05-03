87c4e10e01e3929449f02dd9dff15c347a5e7d68
Krystian <krystian.k.nowak@gmail.com>
2019-05-03 10:43:03 +0200

    anime

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,6 +9,10 @@
         </div>
       </transition-group>
     </draggable>
+
+    <p><strong>Previus Index:</strong>{{oldIndex}}</p>
+    <p><strong>New Index:</strong>{{newIndex}}</p>
+
   </div>
 </template>
 
@@ -52,15 +56,41 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
+strong {
   display: inline-block;
-  margin: 0 10px;
 }
-a {
-  color: #42b983;
+.sortable {
+  width: 100%;
+  background: white;
+  padding: 1em;
+  cursor: move;
+  margin-bottom: 2px;
+
+  span {
+    float: right;
+  }
+}
+
+.hello .sortable-drag {
+  opacity: 0;
+}
+
+.flip-list-move {
+  transition: transform 0.5s;
+}
+
+.ghost {
+  border-left: 6px solid rgb(0,183,255);
+  box-shadow: 10px 10px 5px -1px rgba(0,0,0,0.14);
+  opacity: .7;
+
+  &::before {
+    content: " ";
+    position: absolute;
+    width:20px;
+    height:20px;
+    margin-left: -50px;
+    background-image: url('../assets/drag.svg');
+  }
 }
 </style>
